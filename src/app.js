1f558d780779ec5cbb23a34b2fbc95503c5a860a
Harsh Bhikadiya <harsh.bhikadiya55@gmail.com>
2024-05-05 18:57:03 +0530

    added assetlinks.json

--- src/app.js
+++ src/app.js
@@ -11,30 +11,29 @@ require("./utils/constants");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-//Body parser Middleware
+// Body parser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
-//clear database:
+// clear database:
 app.delete("/", async (req, res) => {
   await clearDatabase();
   res.send("success");
 });
 
-//Routes:
+// Routes:
 app.use("/auth", authRouter);
 app.use("/dm", dmRouter);
 app.use("/server", serverRouter);
-app.use("/chat",chatRouter);
+app.use("/chat", chatRouter);
 
-
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
   res.send("Server is running.");
 });
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
 });
-
 
 module.exports = app;
